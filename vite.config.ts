a311b4999595aea2f0c93b56624656c754bcc2a3
yashwanth1232 <221fa04319@gmail.com>
2025-11-11 11:56:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 Add this line
+  base: './', // 🔥 this is critical
+  build: {
+    outDir: 'dist',
+  },
 })
